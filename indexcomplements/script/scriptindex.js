1658e8d0f4f6a730f99c2bb9c80b95e575604483
Manuel Rico <ManuelRico98@outlook.es>
2019-11-01 21:16:38 -0600

    Cambio en el orden del sistema de archivos

---
+++ indexcomplements/script/scriptindex.js
@@ -0,0 +1,7 @@
+document.getElementById("imagenforo").addEventListener("click",function () {
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.getElementById("cancelar").addEventListener("click",function () {
+    document.querySelector(".popup").style.display = "none";
+})
