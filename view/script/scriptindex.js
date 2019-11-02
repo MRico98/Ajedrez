2bd1af15f526776078e5ee0d1c1fd1da12ea03ca
Manuel Rico <ManuelRico98@outlook.es>
2019-11-01 21:22:55 -0600

    remover folders

--- view/script/scriptindex.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("imagenforo").addEventListener("click",function () {
-    document.querySelector(".popup").style.display = "flex";
-})
-
-document.getElementById("cancelar").addEventListener("click",function () {
-    document.querySelector(".popup").style.display = "none";
-})
