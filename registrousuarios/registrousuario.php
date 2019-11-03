cd358a2567f57857531adc131cc095dc93f8eeb1
Manuel Rico <ManuelRico98@outlook.es>
2019-11-02 21:06:14 -0600

    Avances en el foro

---
+++ registrousuarios/registrousuario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang = "en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta title="Description" content="Pagina para el registro de nuevos usuarios">
+    <meta title="keywords" content="usuarios,ajedrez,registros">
+    <title>Ingresar nuevo usuario</title>
+    <link rel="shorcut icon" href="../indexcomplements/ima/icono.ico">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="estilosregistro.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header class="container-fluid" id="cabeceraprincipal">
+        <picture>
+            <img class="rounded float-left" src="../indexcomplements/ima/icono.jpg" alt="imagen icono" width="100" height="100">
+        </picture>
+        <H1 class="font-weight-bolder" id="tituloprincipal">Registro de nuevo usuario</H1>
+    </header>
+
+    <section id="cuerpoprincipal">
+    dsdsd
+    </section>
+
+</body>
+
+</html>
