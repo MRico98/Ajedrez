c52fa7c641c70e27836b2bc7ba7665e0533976b8
Zeus Sarmiento <zeusjonass@gmail.com>
2019-11-24 12:36:59 -0600

    actualizacion en el index

--- indexcomplements/script/scriptindex.js
+++ indexcomplements/script/scriptindex.js
@@ -2,6 +2,10 @@ document.getElementById("imagenforo").addEventListener("click",function () {
     document.querySelector(".popup").style.display = "flex";
 })
 
+document.getElementById("botonforo").addEventListener("click",function () {
+    document.querySelector(".popup").style.display = "flex";
+})
+
 document.getElementById("cancelar").addEventListener("click",function () {
     document.querySelector(".popup").style.display = "none";
 })
